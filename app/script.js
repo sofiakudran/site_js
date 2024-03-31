e0110224c8f30b0d73d292eee611c35ebba8434a
sofiakudran <161974173+sofiakudran@users.noreply.github.com>
2024-03-31 21:52:40 +0300

    Update script.js

--- app/script.js
+++ app/script.js
@@ -1,16 +1,12 @@
 function getRandomDiscount() {
-    // Генеруємо випадкове число від 10 до 30 (включно)
     return Math.floor(Math.random() * (30 - 10 + 1)) + 10;
 }
 
 function showDiscountAlert() {
-    // Отримуємо випадкове число знижки
     let discount = getRandomDiscount();
-    // Виводимо повідомлення з випадковим числом знижки
     alert("Вам надано знижку " + discount + "%!");
 }
 
-// Функція для руху тексту
 function moveText() {
     let container = document.getElementsByClassName('slide')[0];
     let texts = document.getElementsByClassName('text');
@@ -21,21 +17,16 @@ function moveText() {
         let textWidth = text.offsetWidth;
         let textLeft = parseInt(text.style.left) || 0;
 
-        // Рухаємо текст вліво
         textLeft--;
 
-        // Якщо текст вийшов за межі контейнера, перемістимо його знову на початок
         if (textLeft <= -textWidth) {
             textLeft = containerWidth;
         }
 
-        // Застосовуємо зміщення до тексту
         text.style.left = textLeft + 'px';
     }
 
-    // Запускаємо знову анімацію
     requestAnimationFrame(moveText);
 }
 
-// Починаємо анімацію
 moveText();
